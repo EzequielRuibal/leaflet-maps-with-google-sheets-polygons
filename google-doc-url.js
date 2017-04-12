35561239c8ccb3448434350c7d9ec3b1d01060a8
EzequielRuibal <eze1965@gmail.com>
2017-04-12 13:17:34 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/18rnX-yoX6Q-1TfBghFRrUi2bKm6NJZN_DikAa1KU4UE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pmcfNEmNIgYid0mTyGwOLd0esBHRixqnCx9zsWJd2uE/edit?usp=sharing';
